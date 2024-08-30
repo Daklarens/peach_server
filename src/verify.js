208faf72f0e54e656ca61643004355f50e7ae481
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-08-31 02:09:58 +0300

    kek2

--- src/verify.js
+++ src/verify.js
@@ -5,26 +5,7 @@ require('dotenv').config();
 function verifyTelegramData(initDataString) {
     // Извлекаем секретный токен бота из переменных окружения
     const botToken = process.env.TELEGRAM_BOT_TOKEN;
-    const initData = querystring.parse(initDataString);
-
-    // Если поле user существует и является строкой JSON, преобразуем его обратно в объект
-    if (initData.user && typeof initData.user === 'string') {
-        initData.user = JSON.parse(initData.user);
-    }
-
-    const { hash, ...data } = initData;
-
-    // Если поле user существует как объект, преобразуем его обратно в JSON строку без кодирования
-    if (typeof data.user === 'object') {
-        data.user = JSON.stringify(data.user);
-    }
-
-    // Сортируем ключи и создаем строку для проверки данных с использованием '\n'
-    const sortedKeys = Object.keys(data).sort();
-    const dataCheckString = sortedKeys
-        .map(key => `${key}=${data[key]}`)
-        .join('\n'); // Используем '\n' как разделитель
-
+   const str = 'auth_date=1725058933\nquery_id=AAGYe81ZAAAAAJh7zVk7ct-Y\nuser={id:1506638744,first_name:"Dark",last_name:"Larens",username:"darklarens",language_code:"ru",is_premium:true,allows_write_to_pm:true}'
     // Создаем секретный ключ используя HMAC-SHA256 и строку "WebAppData"
     const secretKey = crypto.createHmac('sha256', botToken)
         .update("WebAppData")
@@ -32,11 +13,11 @@ function verifyTelegramData(initDataString) {
 
     // Генерируем проверочный хеш с использованием секретного ключа
     const checkHash = crypto.createHmac('sha256', secretKey)
-        .update(dataCheckString)
+        .update(str)
         .digest('hex');
 
     // Печатаем для отладки
-    console.log('Строка проверки данных:', dataCheckString);
+    console.log('Строка проверки данных:', secretKey);
     console.log('Сгенерированный хеш:', checkHash);
     console.log('Полученный хеш от Telegram:', hash);
 
