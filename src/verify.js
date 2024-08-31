226adb832ebd40b3f0444e4e00825807b2f32e63
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-08-31 12:22:50 +0300

    kek2

--- src/verify.js
+++ src/verify.js
@@ -7,18 +7,29 @@ function verifyTelegramData(initDataString) {
     const botToken = process.env.TELEGRAM_BOT_TOKEN;
     // Парсим строку initData в объект
     const initData = querystring.parse(initDataString);
+
     // Преобразуем поле user из URL-кодированной строки JSON обратно в объект, затем в строку JSON
     if (initData.user && typeof initData.user === 'string') {
         initData.user = JSON.stringify(JSON.parse(initData.user)); // Декодируем и форматируем
     }
+
+    // Преобразуем строку JSON user в нужный формат с экранированными кавычками
+    if (initData.user) {
+        initData.user = initData.user.replace(/"/g, '\\"'); // Экранируем кавычки
+        initData.user = `{\"${initData.user}\"}`; // Оборачиваем строку user в кавычки и добавляем префикс 'user='
+    }
+
     // Убираем поле hash из данных для проверки
     const { hash, ...data } = initData;
+
     // Сортируем ключи в алфавитном порядке
     const sortedKeys = Object.keys(data).sort();
+
     // Формируем строку в нужном формате с разделителем '\n'
     const formattedString = sortedKeys
         .map(key => `${key}=${data[key]}`)
         .join('\n');  // Используем '\n' как разделитель
+
 
     console.log('Сформированная строка для хеширования:', formattedString);
 
