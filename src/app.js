e7998a4d99274d0a0745784bc48c00f383e9d5f1
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-09-02 22:24:04 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const app = express(); 
+
+const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public'))); // Подключите статические файлы до маршрутов
 app.use(express.json({ limit: '13mb' })); 
 app.use(bodyParser.urlencoded({ extended: true }));
-//Для работы с api
+
+// Для работы с API
 app.use("/api/users", require("./routes/users"));
-//Для отображения страницы
+
+// Для отображения страницы
 app.use("/", require("./routes/auth"));
 
 module.exports = {
