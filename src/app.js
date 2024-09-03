a6d90044e4aa459112e8404529f53ae45222d846
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-09-03 22:25:40 +0300

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use((req, res, next) => {
+  console.log('Incoming Request Headers:', req.headers);
+  req.on('data', chunk => {
+    console.log('Received chunk:', chunk.toString());
+  });
+  req.on('end', () => {
+    console.log('End of request');
+  });
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.json({ limit: '13mb' })); 
