2a6b4b2a0299531c73c9b03ae0a823551d951011
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-09-04 19:34:31 +0300

    update 2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require('path');
+const multer = require('multer');
 const cookieParser = require('cookie-parser');
-const multer  = require("multer");
+const path = require('path');
+
 const app = express();
+
+// Настраиваем multer для загрузки файлов в папку 'uploads'
+const upload = multer({ dest: 'uploads/' });
 
 /*
 app.use((req, res, next) => {
@@ -17,14 +21,27 @@ app.use((req, res, next) => {
 }); 
 */
 
-const upload = multer({ dest: 'uploads/' });
+
+
+// Подключаем multer для обработки загрузки файлов, до обработки JSON и URL-encoded данных
+// 'file' — это имя поля с файлом, нужно уточнить, если другое
 app.use(upload.single('file'));
-app.use(cookieParser());
+
+// Подключаем парсинг JSON и URL-encoded данных
+// Эти middleware должны быть после multer, чтобы не обрабатывать multipart/form-data запросы
 app.use(express.json({ limit: '50mb' })); // Увеличьте лимит в зависимости от ваших нужд
-app.use("/api/users/upload", require("./routes/foto"))
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Подключаем cookie-parser после обработки JSON и URL-encoded
+app.use(cookieParser());
+
+// Подключаем статические файлы, желательно после middleware для обработки данных
+// Добавление '/dist' для доступа к статическим файлам из папки dist
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use(express.static(path.join(__dirname, '../dist')));
+
+// Подключаем маршруты, которые используют multer, json и urlencoded данные
+app.use("/api/users/upload", require("./routes/foto"));
 app.use("/api/users/", require("./routes/users"));
 app.use("/", require("./routes/auth"));
 
