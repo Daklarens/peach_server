fb24acd1a39b9431c3ba99e3b9e3e5d6ddb355a0
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-09-04 11:53:21 +0300

    update 2

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.json({ limit: '13mb' })); 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({limit: '13mb', extended: true}));
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
