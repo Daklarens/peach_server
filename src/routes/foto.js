2626793dfb7fc02f501f9c47812be2d0d4548e14
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-09-04 18:58:08 +0300

    update 2

--- src/routes/foto.js
+++ src/routes/foto.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const multer  = require("multer");
 const router = express.Router();
 
-router.post("/upload", function (req, res, next) {
+const upload = multer({dest:"uploads"});
+router.post("/upload", upload.single("filedata"), function (req, res, next) {
+   
     let filedata = req.file;
+ 
     console.log(filedata);
     if(!filedata)
         res.send("Ошибка при загрузке файла");
