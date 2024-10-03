14957e50bdd690ad6746908226ac6197dcce3bc1
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-10-03 19:47:26 +0300

    upds

--- src/services/servicesUser.js
+++ src/services/servicesUser.js
@@ -88,7 +88,10 @@ class UserService {
   async getAnketsForUser(tid,page){
     const anketUser = await db.find('ankets',{tid})
     if(anketUser.length === 1){
-      const ankets = await db.find('ankets',{sex:anketUser[0].searchsex})
+      const arrActions = await db.find('actions',{uid:tid})
+      const tidArray = arrActions.map(profile => profile.tid);
+      const uniqueTids = [...new Set(tidArray)];
+      const ankets = await db.find('ankets',{sex:anketUser[0].searchsex,tid:{ $nin: uniqueTids }})
       if (page === 0) {
         return ankets.slice(0, 6);  // Отправляем нужную порцию анкет
       } else {
@@ -108,8 +111,7 @@ class UserService {
         tid: action.tid,
         action: action.action
       }));
-      const adder = await db.insertAll('actions',updatedArr)
-      console.log(adder)
+      await db.insertAll('actions',updatedArr)
     }
   }
 
