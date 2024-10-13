7e965bc2125b21f60f0a077c2c009c1e74a61c74
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-10-13 22:27:52 +0300

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -103,8 +103,7 @@ router.get('/f1/:filename', (req, res) => {
 
 router.post('/getPeachUser', (req,res)=>{
   const data = req.body.data
-  console.log(data)
-  bot.sendMessage(data.tid,`Вы выбрали пользователя с id <a href="tg://user?id=${data.ttid}">${data.name}</a>`,{parseMode:'html'})
+  bot.sendMessage(data.tid,`Теперь вы можете начать общение с <a href="tg://user?id=${data.ttid}">${data.ancetName}</a>`,{parseMode:'html'})
 })
 
 bot.start();
