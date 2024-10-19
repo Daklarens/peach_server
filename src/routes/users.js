14cee3851164b5f9a30af80ccf2c061f7787aa81
aleksandr_podryvanov <asasinskrit123@gmail.com>
2024-10-19 23:28:58 +0300

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -74,6 +74,7 @@ router.post('/match', async(req,res)=>{
       // если токен правильный 
       const getMatchUsers = await service.matchAnkets(veryfToken.decoded.id)
       const getMeLikes = await service.getMeLikes(veryfToken.decoded.id)
+      console.log(getMeLikes)
       if(getMatchUsers|| getMeLikes){
         res.send({token:veryfToken.token,data:getMatchUsers,myLike:getMeLikes})
       }else{
